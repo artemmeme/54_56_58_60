cf98970914bea74435a0c3aa675efdc1bc9e8f4c
Артем Шустов <artemmeme2017@yandex.ru>
2022-12-18 16:42:54 +0300

    Программа для отображения координат чисел в трехмерном массиве

--- 60/Program.cs
+++ 60/Program.cs
@@ -1,1 +1,72 @@
-﻿
+﻿// Сформируйте трёхмерный массив из неповторяющихся (HARD - случайных уникальных) двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.WriteLine($"Введите размер массива X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите размер массива Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите размер массива Z:");
+int z = Convert.ToInt32(Console.ReadLine());
+
+
+int[,,] array = new int[x, y, z];
+FillArray(array);
+PrintArray(array);
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} Z({k}); ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,,] array)
+{
+    int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = temp[count];
+                count = count + 1;
+            }
+        }
+    }
+}
+
